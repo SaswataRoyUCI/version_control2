557812ad7b4f03d4d4533590f7338b36e3785b39
Saswata Roy <saswatar@uci.edu>
2015-06-23 09:45:29 -0400

    Temp converted

---
+++ src/temperature.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+"""This is a python  module that converts tmperature
+
+"""
+def f_to_k(temp):
+	converted = ((temp-32.0)*(5.0/9.0))+273.15
+	return converted
+print(f_to_k(212))
+
