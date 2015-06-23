8643ecf49eadedc4e18c85c599fed3fe4fe62681
Saswata Roy <saswatar@uci.edu>
2015-06-23 10:00:32 -0400

    hello. This is fun

--- src/temperature.py
+++ src/temperature.py
@@ -6,5 +6,11 @@
 def f_to_k(temp):
 	converted = ((temp-32.0)*(5.0/9.0))+273.15
 	return converted
-print(f_to_k(212)+"LMAO")
+print(str(f_to_k(212))+"LMAO")
+
+def k_to_c(temp):
+	return temp-273.15
+
+def f_to_c(temp):
+	return (k_to_c(f_to_k(temp)))
 
